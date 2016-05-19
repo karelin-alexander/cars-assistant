00f1a3c99a46422f22ca9dd26f55f624265aa151
karelin-alexander <karelin.sasha@gmail.com>
2016-05-19 14:45:21 +0300

    added carCost

--- Server/app.js
+++ Server/app.js
@@ -15,9 +15,12 @@ var request    = require("request");
 var dt = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') ;
 
 app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.use(morgan('dev'));
+
 app.use(passport.initialize());
 
 mongoose.connect(config.database);
@@ -27,6 +30,7 @@ require('./config/passport')(passport);
 var apiRoutes = express.Router();
 
 apiRoutes.post('/signup', function(req, res) {
+  console.log(req.body.name + ' - ' + req.body.password);
   if (!req.body.name || !req.body.password) {
     res.json({success: false, msg: 'Пожалуйста введите логин и пароль.'});
   } else {
@@ -85,6 +89,22 @@ apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false}), fu
   }
 });
 
+apiRoutes.post('/carCost', function(req, res) {
+  console.log(req.body.mark + ' - ' + req.body.model);
+  var url = "http://auto2.yandex.ru/api/1.0/stats/?rid=213&mark=" + req.body.mark + "&model=" + req.body.model;
+
+  console.log(url);
+
+  request({
+    url: url,
+    json: true
+  }, function (error, response, body) {
+       if (!error && response.statusCode === 200) {
+         console.log(body);
+       }
+  });
+});
+
 apiRoutes.get('/carData', function (req, res) {
   mongoose.model('cars', carsInfo).find(function (err, cars) {
     res.json(cars);
@@ -122,20 +142,6 @@ var carsInfo = new mongoose.Schema({
   img: String,
   date: String
 });
-
-/*var url = "http://auto2.yandex.ru/api/1.0/stats/?rid=213&mark=FORD&model=FOCUS";
-
-request({
-  url: url,
-  json: true
-}, function (error, response, body) {
-
-  if (!error && response.statusCode === 200) {
-    console.log(body);
-  }
-});*/
-
-
 
 app.listen(port, function () {
   console.log(dt + ' - ' + clc.bold('Сервер запущен - 127.0.0.1:' + port));
